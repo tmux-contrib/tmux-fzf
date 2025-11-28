f29830eac0ded44eb0c6972f6161d73a41d13202
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-11-28 16:34:41 +0400

    refactor(tmux): replace fzf-tmux with fzf --tmux flag
    Replace deprecated fzf-tmux -p command with new fzf --tmux=100%,100%
    syntax in project and session selection scripts. Remove border styling
    from project selector for cleaner appearance.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- scripts/tmux-fzf-session.sh
+++ scripts/tmux-fzf-session.sh
@@ -9,7 +9,7 @@ fzf_tmux_session() {
 	local SESSION_NAME
 
 	# List sessions, extract their names, and use fzf to select one.
-	SESSION_NAME=$(tmux ls | cut -d: -f1 | fzf-tmux -p --header=' Session' --bind='ctrl-x:execute(tmux kill-session -t {})+reload(tmux ls | cut -d: -f1),space:jump,jump:accept')
+	SESSION_NAME=$(tmux ls | cut -d: -f1 | fzf --tmux=100%,100% --header=' Session' --bind='ctrl-x:execute(tmux kill-session -t {})+reload(tmux ls | cut -d: -f1),space:jump,jump:accept')
 
 	# If a session is selected, switch to it.
 	if [[ -n $SESSION_NAME ]]; then
